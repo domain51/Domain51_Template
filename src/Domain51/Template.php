4236be63f3bf234465550609996b984c3ed1d4b8
tswicegood <tswicegood@159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b>
2007-07-20 04:09:07 +0000

    Add API docs to code
    git-svn-id: svn://localhost/Domain51_Template/trunk@217 159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b

--- src/Domain51/Template.php
+++ src/Domain51/Template.php
@@ -1,13 +1,65 @@
 <?php
+/**
+ * This file contians {@link Domain51_Template}
+ *
+ * @package Tools
+ * @subpackage Template
+ * @author Travis Swicegood <development@domain51.com>
+ * @version Release: @@VERSION@@
+ * @copyright 2007 Domain51
+ * @license http://www.gnu.org/licenses/lgpl.html LGPL
+ */
 
+/**
+ * Insure that Domain51_Loader is already available
+ *
+ * @ignore
+ */
 require_once 'Domain51/Loader.php';
 
+
+/**
+ * Domain51_Template provides a small, lightweight,
+ * extensible templating engine.
+ * 
+ * Values are assigned by assigning properties to
+ * the template object, and retrieved within the
+ * templates via the $this object.
+ * 
+ * Domain51_Template implements a Value Object
+ * pattern.  As such, once instantiated, the
+ * template object represents on file in the file
+ * system.  There is no limit on how the file is
+ * named, or where it is placed.  The only
+ * criteria is that it must be readable, and when
+ * passed to is_file(), return true.
+ * 
+ * Plug-ins can be any valid PHP callback,
+ * allowing infinite flexibility in configuring
+ * your template object.  Plugins, once
+ * registered, are accessed via the $this object,
+ * either using their method or function name if
+ * no $alias was provided, or the using the name
+ * provided as its $alias when registered.
+ *
+ * @package Tools
+ * @subpackage Template
+ * @author Travis Swicegood <development@domain51.com>
+ * @version Release: @@VERSION@@
+ * @copyright 2007 Domain51
+ * @license http://www.gnu.org/licenses/lgpl.html LGPL
+ */
 class Domain51_Template
 {
     private $_callbacks = array();
     private $_data = array();
     private $_file = null;
     
+    /**
+     * Handle instantiation
+     *
+     * @param string $file A valid file
+     */
     public function __construct($file)
     {
         if (!is_file($file)) {
@@ -28,16 +80,40 @@ class Domain51_Template
         $this->_file = $file;
     }
     
+    /**
+     * Magic method to handle retrieving assigned values to the template object
+     *
+     *
+     * @param string $key
+     * 
+     * @return mixed
+     */
     public function __get($key)
     {
         return $this->_data[$key];
     }
     
+    
+    /**
+     * Magic method to handle assigning values to this template object
+     *
+     *
+     * @param string $key
+     * 
+     * @param mixed $value
+     */
     public function __set($key, $value)
     {
         $this->_data[$key] = $value;
     }
     
+    
+    /**
+     * Magic method to handle converting this object to a string
+     *
+     *
+     * @return string
+     */
     public function __toString()
     {
         ob_start();
@@ -46,6 +122,19 @@ class Domain51_Template
         return $buffer;
     }
     
+    
+    /**
+     * Used to register callbacks to use as plug-ins.
+     *
+     * $callback must be a valid callback as determined by is_callable().  If not null,
+     * $alias will serve as the name used to invoke the callback; if null, the function
+     * or method name will be used to invoke the callback.
+     *
+     *
+     * @param mixed $callback
+     *
+     * @param null|string $alias
+     */
     public function registerCallback($callback, $alias = null)
     {
         if (!is_callable($callback)) {
@@ -62,6 +151,14 @@ class Domain51_Template
         $this->_callbacks[$alias] = $callback;
     }
     
+    /**
+     * Magic method to handle dispatching method calls to registered plug-ins
+     *
+     *
+     * @param string $method
+     *
+     * @param array $arguments
+     */
     public function __call($method, $arguments)
     {
         array_unshift($arguments, $this);
